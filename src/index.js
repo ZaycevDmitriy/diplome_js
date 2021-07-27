778f3cf6063c727afdda3fd757bae7c704254e76
dmitrii_zaitsev <zaycevdi@gmail.com>
2021-07-27 21:18:14 +0300

    сделал плавный скролл на сайте

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import openingModalWindowCallback from "./modules/openingModalWindowCallback";
+import smoothScrollTo from "./modules/smoothScrollTo";
 
 openingModalWindowCallback();
-
+smoothScrollTo();
 
 
 
