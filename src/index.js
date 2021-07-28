36d724817e89189e558a11eac69953405c5744b8
dmitrii_zaitsev <zaycevdi@gmail.com>
2021-07-28 20:50:29 +0300

    Сделал верхний слайдер сайта.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import openingModalWindowCallback from "./modules/openingModalWindowCallback";
 import smoothScrollTo from "./modules/smoothScrollTo";
 import openingMobileMenu from "./modules/openingMobileMenu";
+import topSlider from "./modules/topSlider";
 
 openingModalWindowCallback();
 smoothScrollTo();
 openingMobileMenu();
-
+topSlider();
 
---
+++ src/modules/topSlider.js
@@ -0,0 +1,45 @@
+const topSlider = () => {
+  const slider = document.querySelector('.top-slider'),
+    sliderItems = document.querySelectorAll('.item'),
+    tableItems = document.querySelectorAll('.table');
+  let count = 0;
+
+  const addDots = () => {
+    const listDots = document.createElement('ul');
+    listDots.classList.add('slick-dots');
+    slider.append(listDots);
+
+    for (let i = 0; i < sliderItems.length; i++) {
+      const dot = document.createElement('li');
+      dot.classList.add('dot');
+      if (i === 0) dot.classList.add('slick-active');
+      listDots.append(dot);
+    }
+  };
+
+  addDots();
+
+  const dots = document.querySelectorAll('.dot');
+  console.log(dots);
+
+  const removeClass = (items, nameClass) => {
+    items.forEach(i => {
+      i.classList.remove(nameClass);
+    });
+  };
+
+  const movingSlides = () => {
+    removeClass(sliderItems, 'item-active');
+    removeClass(tableItems, 'active');
+    removeClass(dots, 'slick-active');
+    sliderItems[count].classList.add('item-active');
+    tableItems[count].classList.add('active');
+    dots[count].classList.add('slick-active');
+    count++;
+    if (count >= sliderItems.length) count = 0;
+  };
+
+  setInterval(movingSlides, 3000);
+};
+
+export  default topSlider;
